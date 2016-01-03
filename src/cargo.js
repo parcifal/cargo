eed098ab5838032552cce5ed1a1179ee37b4ea9d
Michaël van de Weerd <com@michaelvandeweerd.net>
2016-01-03 14:03:15 +0100

    Added support for a loading indicator

--- src/cargo.js
+++ src/cargo.js
@@ -1,24 +1,48 @@
 // the initiation methods to be called when the document is ready
 var initiations = [];
 
-// the elements to be appended to the document's head when the document is ready
-var elements = [];
+// the scripts to be appended to the document's head when the document is ready
+var scripts = [];
+
+// the links to be appended to the document's head when the document is ready
+var links = [];
 
 // indicates whether or not the document has loaded all its resources
 var ready = false;
 
-// call all initiation functions and load all elements when the document is
-// ready
+var INDICATOR_CLASSNAME = "cargo-load";
+
+var INDICATOR_LABEL_INITIATION = "calling %1()";
+var INDICATOR_LABEL_SCRIPT = "loading script %1";
+var INDICATOR_LABEL_LINK = "loading resource %1";
+
+// call all initiation functions and load all scripts and links when the
+// document is ready
 window.onload = function() {
 	ready = true;
 
+	initiateIndicators(initiations.length + scripts.length + links.length);
+
 	for (var i = 0; i < initiations.length; i++) {
+		updateIndicators(INDICATOR_LABEL_INITIATION.replace("%1",
+				initiations[i].name));
+
 		initiations[i]();
 	}
 
-	for (var i = 0; i < elements.length; i++) {
-		document.head.appendChild(elements[i]);
+	for (var i = 0; i < scripts.length; i++) {
+		updateIndicators(INDICATOR_LABEL_SCRIPT.replace("%1", scripts[i].src));
+
+		document.head.appendChild(scripts[i]);
 	}
+
+	for (var i = 0; i < links.length; i++) {
+		updateIndicators(INDICATOR_LABEL_LINK.replace("%1", links[i].href));
+
+		document.head.appendChild(links[i]);
+	}
+	
+	finishIndicators();
 };
 
 /**
@@ -53,7 +77,11 @@ function loadScript(src, async) {
 		script.async = async;
 	}
 
-	loadElement(script);
+	if (ready) {
+		document.head.appendChild(script);
+	} else {
+		scripts.push(script);
+	}
 }
 
 /**
@@ -73,19 +101,52 @@ function loadLink(href, type, rel) {
 	link.type = type;
 	link.rel = rel;
 
-	loadElement(link);
+	if (ready) {
+		document.head.appendChild(link);
+	} else {
+		links.push(link);
+	}
 }
 
-/**
- * Load the specified element by appending it to the head of the document.
- * 
- * @param element
- *            The element to be appended to the head of the document.
- */
-function loadElement(element) {
-	if (ready) {
-		document.head.appendChild(element);
-	} else {
-		elements.push(element);
+function initiateIndicators(size) {
+	var indicators = document.getElementsByClassName(INDICATOR_CLASSNAME);
+
+	for (var i = 0; i < indicators.length; i++) {
+		indicators[i].dataset.cargoSize = size;
+		indicators[i].dataset.cargoStep = 0;
+
+		indicators[i].classList.remove("idle");
+		indicators[i].classList.add("active");
+	}
+}
+
+function updateIndicators(label) {
+	var indicators = document.getElementsByClassName(INDICATOR_CLASSNAME);
+
+	for (var i = 0; i < indicators.length; i++) {
+		var step = parseInt(indicators[i].dataset.cargoStep);
+		var size = parseInt(indicators[i].dataset.cargoSize);
+		
+		step++;
+
+		indicators[i].dataset.cargoStep = step;
+		indicators[i].dataset.cargoLabel = label;
+		indicators[i].dataset.cargoPercentageDone = (step / size) * 100;
+		indicators[i].dataset.cargoPercentageLeft = 100 - (step / size) * 100;
+	}
+}
+
+function finishIndicators() {
+	var indicators = document.getElementsByClassName(INDICATOR_CLASSNAME);
+
+	for (var i = 0; i < indicators.length; i++) {
+		delete indicators[i].dataset.cargoSize;
+		delete indicators[i].dataset.cargoStep;
+		delete indicators[i].dataset.cargoLabel;
+		delete indicators[i].dataset.cargoPercentageDone;
+		delete indicators[i].dataset.cargoPercentageLeft;
+
+		indicators[i].classList.remove("active");
+		indicators[i].classList.add("idle");
 	}
 }
